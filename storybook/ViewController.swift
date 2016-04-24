a37ca6b915179c5e994141dff64d0aed1a53055a
lighterletter <jgomez818@gmail.com>
2016-04-24 08:52:17 -0400

    initial commit

--- storybook/ViewController.swift
+++ storybook/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import WatsonDeveloperCloud
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,54 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
-}
+    
+    
+//    
+//    let service = TextToSpeech(username: "yourname", password: "yourpass")
+//    
+//    //synthesize the audio
+//    service.synthesize("Hello World", oncompletion: {
+//    data, error in
+//    if let data = data {
+//    
+//    //play the audio
+//    let audioPlayer = try AVAudioPlayer(data: data)
+//    audioPlayer.prepareToPlay()
+//    audioPlayer.play()
+//    }
+//    )
+    
+    @IBAction func readStory(sender: AnyObject) {
+        
+      //  BasicAuthenticationStrategy("zzmehdi@gmail.com")
+        
+     
+        
+      let service = TextToSpeech(username: "0a750766-2074-424b-8238-bf347d5d7a81", password: "XckmUdVX2pzH")
+        
+    
+        
+        //synthesize the audio
+        service.synthesize("Hello World") { (data, error) in
+            
+                if let audio = data {
+                    
+                     do {
+                    //play the audio
+                        self.player = try AVAudioPlayer(data: audio)
+                        print(data)
+                        self.player!.prepareToPlay()
+                       // audioPlayer.delegate = self
+                        self.player!.play()
+                     } catch {
+                        print("Couldn't create player")
+                    }
+                } else {
+                    print(error)
+                }
+            }
+        }
+  
+    }
+    
 
